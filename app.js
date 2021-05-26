372884a26bf4e3a1cb18427658ab9b80ac273797
OmBelote <ombelote@gmail.com>
2021-05-26 22:42:07 +0530

    js-complete

--- app.js
+++ app.js
@@ -5,8 +5,28 @@ var txtbox = document.querySelector("#txt-box")
 // var textbox = document.querySelector('#input-btn')
 // var textbox = document.querySelector('input[name="translator"]')
 var output = document.querySelector("#output")
+
+function getTranslationURL(txtbox){
+    return "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"+"?"+"text="+txtbox.value;
+};
+
+function errorHandler(error){
+    console.log('error occured:', error);
+    alert('something wrong with ur server, try again after some time');
+};
+
 function clickHandler(){
-    output.innerHTML = "arrrrr " + txtbox.value;
+    // output.innerHTML = "arrrrr " + txtbox.value;
+    fetch(getTranslationURL(txtbox))
+    .then(response =>response.json())
+    .then(json=>{
+        var translated_text= json.contents.translated;
+        output.innerHTML=translated_text;
+        })
+    .catch(errorHandler);
 };
 
 btnTranslate.addEventListener("click", clickHandler)
+// Fething a api through javascript
+// url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text= I am Om
+// fetch(url).them(response=>response.json()).then(json=>console.log(json))
