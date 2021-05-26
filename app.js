5bc2da8129b8458c75f5980a493cf9f68abce513
OmBelote <ombelote@gmail.com>
2021-05-26 14:20:07 +0530

    i/p o/p processing done

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtbox = document.querySelector("#txt-box")
-
+// var textbox = document.querySelector("textarea")
+// var textbox = document.querySelector('.btn-primary')
+// var textbox = document.querySelector('#input-btn')
+// var textbox = document.querySelector('input[name="translator"]')
+var output = document.querySelector("#output")
 function clickHandler(){
-    console.log("input" , txtbox.value)
-    console.log("clicked");
+    output.innerHTML = "arrrrr " + txtbox.value;
 };
 
 btnTranslate.addEventListener("click", clickHandler)
